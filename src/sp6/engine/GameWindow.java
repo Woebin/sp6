faf98ca2b5f290d7b9458b56ae5d65ebe93e77e6
Theo Walther <woebin@gmail.com>
2016-01-21 21:56:38 +0100

    Basic GUI work started on Gamewindow.

--- src/sp6/engine/GameWindow.java
+++ src/sp6/engine/GameWindow.java
@@ -1,9 +1,47 @@
 package sp6.engine;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
 /**
  * 
  * Hela spelets fönster. Har en/flera menyer. Hostar GameWorld
  *
  */
-public class GameWindow {
-
+public class GameWindow extends javax.swing.JFrame {
+	private JPanel gameDisplay;
+	private JMenu mFile = new JMenu("File");
+	private JMenuItem mNewGame = new JMenuItem("New Game");
+	private JMenuItem mOptions = new JMenuItem("Options");
+	private JMenuItem mExit = new JMenuItem("Exit");
+	private JMenuBar menu = new JMenuBar();
+	
+	GameWindow(){
+		setJMenuBar(this.menu);
+		this.menu.add(this.mFile);
+		this.mFile.add(this.mNewGame);
+		this.mFile.add(this.mOptions);
+		this.mFile.add(this.mExit);
+		
+		this.gameDisplay = new JPanel();
+		this.gameDisplay.setLayout(null);
+		add(this.gameDisplay, "Center");
+		
+		setVisible(true);
+		pack();
+	}
+	
+	class MenuListener implements java.awt.event.ActionListener{
+		MenuListener(){}
+		
+		public void actionPerformed(ActionEvent ae){
+			if(ae.getSource() == GameWindow.this.mNewGame){
+				// Start a new game.
+			}
+			
+		}
+	}
+	
+	public static void main(String[] args){
+		new GameWindow();
+	}
 }
