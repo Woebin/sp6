8998f44ee482961a0862724d70e0a635662ee70d
Jarvitalo <sebastian@lowtech.nu>
2016-01-27 11:40:03 +0100

    Boiler plate code for reversed responsibility of baseobject update through components

--- src/sp6/engine/BaseObject.java
+++ src/sp6/engine/BaseObject.java
@@ -1,10 +1,30 @@
 package sp6.engine;
+
+import sp6.engine.controller.Component;
+
+import java.util.List;
+
 /**
  * 
- * Basobjektet för alla interagerbara objekt i en level. Frågan är om det behövs någon uppdelning? Sätter man 
- * egenskaperna på ett object vart efter så skulle man kunna klara sig bara med ett BaseObject
+ * Basobjektet fÃ¶r alla interagerbara objekt i en level. Frï¿½gan ï¿½r om det behï¿½vs nï¿½gon uppdelning? Sï¿½tter man
+ * egenskaperna pÃ¥ ett object vart efter sï¿½ skulle man kunna klara sig bara med ett BaseObject
  *
  */
-public class BaseObject {
+public abstract class BaseObject {
+    private final List<Component> components;
 
+    public BaseObject(List<Component> components) {
+        this.components = components;
+    }
+
+    public void update(Component.Prio prio, double deltaTime) {
+        for (Component component : components) {
+            System.out.println("Update BaseObject prio: " + prio);
+
+            if (component.getPrio() == prio) {
+                System.out.println("Update BaseObject equals prio: " + prio);
+                component.update(this, deltaTime);
+            }
+        }
+    }
 }
